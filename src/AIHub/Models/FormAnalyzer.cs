769d55a9f5649b877f7eb470b43ec59c5c0e4a49
heblasco <heblasco@microsoft.com>
2023-11-23 12:15:48 +0100

    Form Analyzer added

---
+++ src/AIHub/Models/FormAnalyzer.cs
@@ -0,0 +1,13 @@
+namespace MVCWeb.Models;
+
+public class FormAnalyzerModel{
+    
+    public int? Severity { get; set; }
+    public int? Violence { get; set; }
+    public int? SelfHarm { get; set; }
+    public int? Hate { get; set; }
+    public string? Prompt { get; set; }
+    public string? Image { get; set; }
+    public string? Message { get; set; }
+
+}
