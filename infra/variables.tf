df2051f627afbd414b6a2c3b23ecb2bc9dcdcd69
cmendible <266546+cmendible@users.noreply.github.com>
2024-04-11 11:45:27 +0200

    Bump Docker image versions to 1.0.9 in variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -91,15 +91,15 @@ variable "ca_chat_image" {
 }
 
 variable "ca_prep_docs_image" {
-  default = "ghcr.io/azure/aihub/aihub-prepdocs:1.0.8"
+  default = "ghcr.io/azure/aihub/aihub-prepdocs:1.0.9"
 }
 
 variable "ca_plugin_image" {
-  default = "ghcr.io/azure/aihub/aihub-plugin:1.0.8"
+  default = "ghcr.io/azure/aihub/aihub-plugin:1.0.9"
 }
 
 variable "ca_aihub_image" {
-  default = "ghcr.io/azure/aihub/aihub:1.0.8"
+  default = "ghcr.io/azure/aihub/aihub:1.0.9"
 }
 
 variable "use_random_suffix" {
