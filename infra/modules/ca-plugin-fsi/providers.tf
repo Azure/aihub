876ee11ce242fc6f6a57484e499c8134f90234cb
Roberto Arocha <rarocha@microsoft.com>
2024-05-08 10:56:08 +0200

    tf module added

---
+++ infra/modules/ca-plugin-fsi/providers.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = ">= 1.4.6"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.87.0"
+    }
+    azapi = {
+      source = "Azure/azapi"
+    }
+  }
+}
---
+++ infra/modules/ca-plugin-fsi/variables.tf
@@ -0,0 +1,13 @@
+variable "resource_group_name" {}
+variable "resource_group_id" {}
+variable "location" {}
+variable "func_name" {}
+variable "image_name" {}
+variable "cae_id" {}
+variable "cae_default_domain" {}
+variable "appi_instrumentation_key" {}
+variable "openai_key" {}
+variable "openai_model" {}
+variable "openai_endpoint" {}
+variable "bing_endpoint" {}
+variable "bing_key" {}
