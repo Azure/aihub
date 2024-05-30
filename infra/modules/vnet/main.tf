a9231a43bf64e5a75f9a28c7888c45e38c448d62
Rodrigo Liberoff Vázquez <rliberoff@encamina.com>
2024-05-30 16:02:00 +0000

    - Increased the size of the subnet for Private Endpoints, since currently near 9 services requires them. - Increased the size of the subnet for the APIM as recommended by Microsoft's documentation. - Also, added delegation in the subnet for the APIM as specified by Microsoft's documentation.

--- infra/modules/vnet/main.tf
+++ infra/modules/vnet/main.tf
@@ -9,14 +9,24 @@ resource "azurerm_subnet" "apim" {
   name                 = "snet-apim"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.5.0.0/29"]
+  address_prefixes     = ["10.5.0.0/27"] # A minimum subnet size of /26 or /27 is recommended when creating a new subnet for APIM. Reference: https://learn.microsoft.com/en-us/azure/api-management/integrate-vnet-outbound#prerequisites
+  # Delegate the subnet to "Microsoft.Web/serverFarms". Reference: https://learn.microsoft.com/en-us/azure/api-management/integrate-vnet-outbound#delegate-the-subnet
+  delegation {
+    name = "apim-delegation"
+    service_delegation {
+      name = "Microsoft.Web/serverFarms"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+      ]
+    }
+  }
 }
 
 resource "azurerm_subnet" "private_endpoints" {
   name                 = "snet-pe"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.5.1.0/29"]
+  address_prefixes     = ["10.5.1.0/28"] # Currently, this deployment creates around 9 services that requires private endpoints.
 }
 
 resource "azurerm_subnet" "cae" {
