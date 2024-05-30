384e17d63fb4b6d20117551275dfd5b3d1ea3b40
Rodrigo Liberoff Vázquez <rliberoff@encamina.com>
2024-05-30 16:38:48 +0000

    Add support for private endpoints in Document Intelligence (a.k.a. `Forms`) module.

--- infra/modules/form/variables.tf
+++ infra/modules/form/variables.tf
@@ -1,3 +1,7 @@
 variable "resource_group_name" {}
 variable "location" {}
 variable "form_recognizer_name" {}
+variable "allowed_ips" {}
+variable "vnet_id" {}
+variable "private_endpoints_subnet_id" {}
+variable "use_private_endpoints" {}
