fffe100c296c1a0ba949eae87464bd8a2c6e646b
Rodrigo Liberoff Vázquez <rliberoff@encamina.com>
2024-06-12 10:07:02 +0200

    Add `prevent_deletion_if_contains_resources` flag as `false` to mitigate an open bug in Terraform. or instance, the Resource Group is not deleted when a `Failure Anomalies` resource is present. Reference: https://github.com/hashicorp/terraform-provider-azurerm/issues/18026

--- infra/providers.tf
+++ infra/providers.tf
@@ -32,6 +32,12 @@ provider "azurerm" {
     api_management {
       purge_soft_delete_on_destroy = true
     }
+    resource_group {
+      # This flag is set to mitigate an open bug in Terraform. For instance, the Resource Group is not deleted when a `Failure Anomalies` resource is present.
+      # As soon as this is fixed, we should remove this.
+      # Reference: https://github.com/hashicorp/terraform-provider-azurerm/issues/18026
+      prevent_deletion_if_contains_resources = false
+    }
   }
 }
 
