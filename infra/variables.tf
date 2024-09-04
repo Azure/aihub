faddc770f26f49fee7766236a55d72db4adb9cca
cmendible <266546+cmendible@users.noreply.github.com>
2024-09-04 10:38:30 +0200

    chore: update image versions

--- infra/variables.tf
+++ infra/variables.tf
@@ -103,19 +103,19 @@ variable "ca_chat_image" {
 }
 
 variable "ca_prep_docs_image" {
-  default = "ghcr.io/azure/aihub/aihub-prepdocs:1.0.12"
+  default = "ghcr.io/azure/aihub/aihub-prepdocs:1.0.13"
 }
 
 variable "ca_plugin_image" {
-  default = "ghcr.io/azure/aihub/aihub-plugin:1.0.12"
+  default = "ghcr.io/azure/aihub/aihub-plugin:1.0.13"
 }
 
 variable "ca_fsi_plugin_image" {
-  default = "ghcr.io/azure/aihub/aihub-fsiplugin:1.0.12"
+  default = "ghcr.io/azure/aihub/aihub-fsiplugin:1.0.13"
 }
 
 variable "ca_aihub_image" {
-  default = "ghcr.io/azure/aihub/aihub:1.0.12"
+  default = "ghcr.io/azure/aihub/aihub:1.0.13"
 }
 
 variable "use_random_suffix" {
